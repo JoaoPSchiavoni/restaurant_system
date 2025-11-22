de21f18c7903b43654cbdbd1d7c6b54c55a2b881
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-11-22 17:06:17 -0300

    Add SQLAlchemy models and Alembic migration setup
    Introduced User, Order, and OrderItem models in Models.py and set up Alembic for database migrations, including initial migration scripts and configuration files. Updated README with installation and migration instructions, and added the initial banco.db SQLite database.

--- Models.py
+++ Models.py
@@ -1,2 +1,68 @@
-import sqlalchemy as sa
+from sqlalchemy import create_engine, Column, String, Integer, Float, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
 
+#cria a conexao do seu banco
+db = create_engine("sqlite:///banco.db")
+
+#cria a base do banco de dados
+Base = declarative_base()
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String)
+    email = Column("email", String, nullable=False)
+    password = Column("password", String)
+    active = Column("active", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, name, email, password, active=True, admin=False):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.active = active
+        self.admin = admin
+
+
+class order(Base):
+    __tablename__ = "orders"
+
+    # ORDER_STATUS = (
+    #    ("PENDENTE", "PENDENTE"),
+    #    ("CANCELADO", "CANCELADO"),
+    #    ("FINALIZADO", "FINALIZADO")
+    # )
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String) 
+    user = Column("user", ForeignKey("users.id"))
+    price = Column("price", Float)
+    # items = 
+
+    def __init__(self, user, status="PENDENTE", price=0):
+        self.user = user
+        self.status = status
+        self.price = price
+
+
+class OrderItem(Base):
+    __tablename__ = "order_item"
+
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    amount = Column("amout", Integer)
+    flavor = Column("flavor", String)
+    size = Column("size", String)
+    unit_price = Column("unit_price", Float)
+    order = Column("order", ForeignKey("orders.id"))
+
+    def __init__(self, amount, flavor, size, init_price, order):
+        self.amount = amount
+        self.flavor = flavor
+        self.size = size
+        self.init_price = init_price
+        self.order = order
+# executa a criacao dos metadados do banco (cria efetivamente o banco de. dados)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # restaurant_system
 
+
+pip install fastapi uvicorn sqlalchemy "passlib[bcrypt]" "python-jose[cryptography]" python-dotenv python-multipart
+pip install alembic "alembic init alembic", alembic revision --autogenerate -m "Initial Migration", alembic upgrade head
+pip install sqlalchemy_utils  
